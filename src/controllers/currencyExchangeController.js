f5dbc3e89c7ac20c069adc7bd329c629f43574c3
Parshantrao <parshantrao2017@gmail.com>
2023-04-06 13:29:54 +0530

    changes

--- src/controllers/currencyExchangeController.js
+++ src/controllers/currencyExchangeController.js
@@ -7,8 +7,8 @@ const exchangeRate = async function (req, res) {
 
     let options1 = {
       method: "GET",
-      url: `https://api.apilayer.com/currency_data/convert?to=${to}&from=${from}&amount=1`,
-      headers: { apikey: "XAo4TQmi9PvOeDLwMrFJknHHFH69eUhy" },
+      url: `https://www.xe.com/currencyconverter/convert/?Amount=1&From=${from}&To=${to}`,
+      
     };
 
     let option2 = {
@@ -20,26 +20,30 @@ const exchangeRate = async function (req, res) {
       method: "GET",
       url: `https://www.calculator.net/currency-calculator.html?eamount=1&efrom=${from}&eto=${to}&type=1&x=70&y=10`,
     };
-
+    
     let result1 = await axios(options1);
-    result1 = result1.data.result;
+    const html = result1.data;
+    const $1 = cheerio.load(html);
+    const exchangeRate = parseFloat($1('.result__BigRate-sc-1bsijpp-1').text().split(" ")[0].replace(",",""))
+   
 
     let result2 = await axios(option2);
     const $result2 = cheerio.load(result2.data);
-    result2 = parseFloat($result2(".ccOutputRslt").text());
+    result2 =Number((($result2(".ccOutputRslt").text()).split(" ")[0]).replace(",",""));
+ 
+
 
     let result3 = await axios(options3);
-
     const $ = cheerio.load(result3.data);
     const result = $(".verybigtext").text();
-    const value = parseFloat(result.split(" ")[3]);
+    const value = parseFloat(result.split(" ")[3].replace(",",""));
 
     let arr = [
-      { exchange_rate: result1, source: "https://api.apilayer.com" },
+      { exchange_rate: exchangeRate, source: "https://api.apilayer.com" },
       { exchange_rate: result2, source: "http://www.x-rates.com" },
       { exchange_rate: value, source: "https://www.calculator.net" },
     ];
-    console.log(arr);
+    
 
     return res.status(200).send({ status: true, data: arr });
   } catch (err) {
@@ -65,8 +69,8 @@ const convertCurrency = async function (req, res) {
     let min = Math.min(...currencyRateArr);
 
     let resData = {
-      max_value: Math.abs(amount * max),
-      min_value: Math.abs(amount * min),
+      max_value: Math.round(Math.abs(amount * max)*1000)/1000,
+      min_value: Math.round(Math.abs(amount * min)*1000)/1000,
     };
     return res.status(200).send({ status: true, data: resData });
   } catch (err) {
